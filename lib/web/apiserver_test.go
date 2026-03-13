e8746d2bcbec4ca87308ce72158bca9a07dd5381
Dan Share <daniel.share@goteleport.com>
2026-03-13 12:40:23 +0000

    Undo testing test changes

--- lib/web/apiserver_test.go
+++ lib/web/apiserver_test.go
@@ -10250,11 +10250,12 @@ func TestWebSocketAuthenticateRequest(t *testing.T) {
 			serverExpectError: "i/o timeout",
 			token:             pack.session.Token,
 			readTimeout: func() {
-				<-time.After(wsIODeadline + time.Second)
+				<-time.After(wsIODeadline * 3)
 			},
 		},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 			server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 				sctx, ws, err := proxy.handler.handler.AuthenticateRequestWS(w, r)
 				if err != nil {
